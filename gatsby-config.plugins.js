4ed152ecdfe74af69ae8b0824619d414d44e6562
SurKM9 <msuryakiran09@outlook.com>
2021-06-27 21:17:56 +0200

    add new gtag analytics ID add google gtag analytics plugin remove old google analytics plugin

--- gatsby-config.plugins.js
+++ gatsby-config.plugins.js
@@ -1,6 +1,27 @@
 const config = require('./config');
 
 module.exports = [
+  {
+    resolve: `gatsby-plugin-google-gtag`,
+    options: {
+      // You can add multiple tracking ids and a pageview event will be fired for all of them.
+      trackingIds: [
+        "G-MZ2RZE93ZM", // Google Analytics / GA         
+      ],
+      // This object gets passed directly to the gtag config command
+      // This config will be shared across all trackingIds
+      gtagConfig: {
+        optimize_id: "OPT_CONTAINER_ID",
+        anonymize_ip: true,
+        cookie_expires: 0,
+      },
+      // This object is used for configuration specific to this plugin
+      pluginConfig: {
+        // Puts tracking script in the head instead of the body
+        head: true,
+      },
+    },
+  },
   'gatsby-plugin-react-helmet',
   'gatsby-transformer-sharp',
   'gatsby-plugin-sharp',
@@ -71,16 +92,7 @@ module.exports = [
     options: {
       javascriptEnabled: true,
     },
-  },
-  {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      // The property ID; the tracking code won't be generated without it
-      trackingId: config.googleAnalyticTrackingId,
-      // Defines where to place the tracking script - `true` in the head and `false` in the body
-      head: false,
-    },
-  },
+  },    
   {
     resolve: 'gatsby-plugin-nprogress',
     options: {
